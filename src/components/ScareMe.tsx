888184907c000e08ed624e7b5620988bf4265adf
Earle-Poole <eepoole4@gmail.com>
2020-10-22 23:05:41 -0500

    Finished implementing the logic behind the Scare Me! section of the website.

--- src/components/ScareMe.tsx
+++ src/components/ScareMe.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import './css/scareMe.css'
 
 const techSupportGoreURL =
   'https://www.reddit.com/r/techsupportgore/random.json'
@@ -17,19 +18,44 @@ const getRandomTechSupportGorePost = async () => {
     'randomRedditPost?.[0]?.children?.[0]: ',
     !!randomRedditPost?.[0]?.data?.children?.[0]
   )
-  
+
   if (
     randomRedditPost?.[0]?.data?.children?.[0] &&
-    !randomRedditPost[0].data.children[0].data.is_gallery
+    (!randomRedditPost[0].data.children[0].data.gallery_data ||
+      !randomRedditPost[0].data.children[0].data.gallery_data.items[0].media_id)
   ) {
     getRandomTechSupportGorePost()
+  } else {
+    const postData = randomRedditPost[0].data.children[0].data
+    const mediaID = postData.gallery_data.items[0].media_id
+
+    const img = document.createElement('img')
+    const span = document.createElement('span')
+
+    span.innerHTML = 'Click the image to get another random spook!'
+    img.setAttribute(
+      'src',
+      postData.media_metadata[mediaID].s.u.split('amp;').join('')
+    )
+    img.onclick = () => {
+      getRandomTechSupportGorePost()
+    }
+    const scareMe = document.querySelector('.Scare-Me')
+
+    if (scareMe) {
+      scareMe.innerHTML = ''
+      scareMe.appendChild(img)
+      scareMe.appendChild(span)
+    }
   }
 }
 
 const ScareMe = () => {
-  getRandomTechSupportGorePost()
+  useEffect(() => {
+    getRandomTechSupportGorePost()
+  }, [])
 
-  return <></>
+  return <div className='Scare-Me'></div>
 }
 
 export default ScareMe
