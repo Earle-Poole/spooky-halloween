0e012853c6a1d2bcc05f6313fae957e0d3f4adbb
Udi Levy <webdevudi@gmail.com>
2021-10-16 20:46:23 -0500

    Linked each movie to search imdb with the title

--- src/components/microComponents/MovieTile.tsx
+++ src/components/microComponents/MovieTile.tsx
@@ -9,8 +9,15 @@ interface MovieTileProps {
   };
 }
 const MovieTile = ({ movieStats }: MovieTileProps) => {
+  function searchIMDB(movieName: string) {
+    window.open(
+      `https://www.imdb.com/find?q=${encodeURIComponent(
+        movieName
+      )}&s=tt&exact=true`
+    );
+  }
   return (
-    <div className="movieCard">
+    <div className="movieCard" onClick={() => searchIMDB(movieStats.title)}>
       <img width="175px" height="275px" src={movieStats.url} alt="" />
       <div className="movieContainer">
         <span className="movieStatsTitle">{movieStats.title}</span>
